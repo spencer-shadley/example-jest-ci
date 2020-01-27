cb2411f5674a32a21825c60c5b66831b57552558
Spencer Shadley <spencershadley@utexas.edu>
2020-01-27 13:34:21 -0800

    added three number test case

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -5,8 +5,24 @@ const add = require('../arithmetic');
 describe('Arithmetic', () => {
   describe('Add', () => {
     it('should add two numbers', () => {
-      const actual = add(1, 2);
-      expect(actual).toEqual(3);
+      const numA = 1;
+      const numB = 2;
+      const expected = numA + numB;
+
+      const actual = add(numA, numB);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should add three numbers', () => {
+      const numA = 1;
+      const numB = 2;
+      const numC = 3;
+      const expected = numA + numB + numC;
+
+      const actual = add(numA, numB, numC);
+
+      expect(actual).toEqual(expected);
     });
   });
 });
